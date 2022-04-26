db95e7e39b6d121eaa92df71c6f11490bcf9d7e1
Shreyansh-Mishra <201951146@iiitvadodara.ac.in>
2022-04-26 18:10:27 +0530

    create update user api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var user = require('../models/user');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/update', function(req, res) {
+  if(req.user){
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password;
+    let user = User.findOne({"username": req.user.username});
+    user.username = username;
+    user.email = email;
+    user.password = password;
+    user.save();
+  }
 });
 
 module.exports = router;
